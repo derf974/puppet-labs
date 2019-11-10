e85322b83b54d8ad6bd5b677b0b536b8764b3039
derf <contact@derf.com>
2019-11-10 16:02:41 +0100

    add web and db

--- master/site.pp
+++ master/site.pp
@@ -4,3 +4,27 @@ node default {
         content => "toto is alive",
   }
 }
+
+node web {
+
+    package { 'apache2':
+        ensure => "present",
+        name => "apache2",
+    }
+
+    service { 'apache2':
+        ensure => 'running',
+    }
+
+}
+
+node db {
+    package { 'mysql-server':
+        ensure => 'present',
+        name => 'mysql-server',
+    }
+
+    service { 'mysql':
+        ensure => 'running',
+    }
+}
